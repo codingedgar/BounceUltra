d677c09415b7698a140613f1c34f3ab6ae8e59d6
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-14 01:40:02 -0430

    game manager

---
+++ Bounce/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    #region Variables
+
+    #endregion
+    public static GameManager Instance;
+
+    #region Methods
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+}
