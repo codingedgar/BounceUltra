377d00f0a14ff80438bc833ba83334c049612339
Edgar JRG <erodriguez@teravisiongames.com>
2015-11-18 23:32:06 -0430

    Loading Scene;

---
+++ Bounce/Assets/GameInitializer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameInitializer : MonoBehaviour 
+{
+	#region Variables
+	
+	#endregion
+	
+	#region Methods
+	
+	#endregion
+}
